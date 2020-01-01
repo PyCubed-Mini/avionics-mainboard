84ab7659e55230d8c5360448b60af4baaae6a95e
Max Holliday <maholli@stanford.edu>
2020-01-01 12:45:39 -0800

    adding v01 + repo overhaul

--- README.md
+++
@@ -1,7 +0,0 @@
-# avionics-motherboard
-Main flight computer board for the PyCubed-1 spacecraft
-
-Snapshot: 2019-10-22 maholli
-<p align="middle">
-  <img width="600" src="https://github.com/spacecraft-design-lab-2019/avionics-motherboard/blob/master/Motherboard/mainboard.svg">
-</p>
---
+++ firmware/pycubedminiv01/mpconfigboard.mk
@@ -0,0 +1,19 @@
+LD_FILE = boards/samd51x19-bootloader.ld
+USB_VID = 0x04D8
+USB_PID = 0xED94
+USB_PRODUCT = "pycubed-mini"
+USB_MANUFACTURER = "maholli"
+
+CHIP_VARIANT = SAMD51G19A
+CHIP_FAMILY = samd51
+
+QSPI_FLASH_FILESYSTEM = 0
+INTERNAL_FLASH_FILESYSTEM = 1
+LONGINT_IMPL = MPZ
+
+# Not needed.
+CIRCUITPY_AUDIOBUSIO = 0
+CIRCUITPY_DISPLAYIO = 0
+
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_BusDevice
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
